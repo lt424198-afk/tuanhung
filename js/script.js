1d21d76384e0d14a83ef8ededc31a5f9098a2255
lt424198-afk <lt424198@gmail.com>
2026-03-17 03:14:52 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -70,3 +70,24 @@ function toggleMode(){
 document.body.classList.toggle("dark")
 
 }
+function toggleMode(){
+
+document.body.classList.toggle("dark")
+
+if(document.body.classList.contains("dark")){
+
+localStorage.setItem("mode","dark")
+
+}else{
+
+localStorage.setItem("mode","light")
+
+}
+
+}
+
+if(localStorage.getItem("mode")==="dark"){
+
+document.body.classList.add("dark")
+
+}
